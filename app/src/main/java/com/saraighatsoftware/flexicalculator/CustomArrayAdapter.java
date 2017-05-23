9c98fa5aaa6040ea6b97e7a7ebbc891b3a669387
Abhijeet Baruah <abhijeet080808@gmail.com>
2017-05-24 00:44:10 +0530

    Store spinner font size in dimens.xml

--- app/src/main/java/com/saraighatsoftware/flexicalculator/CustomArrayAdapter.java
+++ app/src/main/java/com/saraighatsoftware/flexicalculator/CustomArrayAdapter.java
@@ -3,6 +3,7 @@ package com.saraighatsoftware.flexicalculator;
 import android.content.Context;
 import android.graphics.Typeface;
 import android.support.annotation.NonNull;
+import android.util.TypedValue;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -12,35 +13,42 @@ import java.util.List;
 
 class CustomArrayAdapter extends ArrayAdapter<String> {
 
+    private Context mContext;
     private final Typeface mTypeface;
 
     CustomArrayAdapter(Context context, int resource) {
         super(context, resource);
+        mContext = context;
         mTypeface = Typeface.createFromAsset(getContext().getAssets(),  "fonts/Teko-Regular.ttf");
     }
 
     CustomArrayAdapter(Context context, int resource, int textViewResourceId) {
         super(context, resource, textViewResourceId);
+        mContext = context;
         mTypeface = Typeface.createFromAsset(getContext().getAssets(), "fonts/Teko-Regular.ttf");
     }
 
     CustomArrayAdapter(Context context, int resource, String[] objects) {
         super(context, resource, objects);
+        mContext = context;
         mTypeface = Typeface.createFromAsset(getContext().getAssets(), "fonts/Teko-Regular.ttf");
     }
 
     CustomArrayAdapter(Context context, int resource, int textViewResourceId, String[] objects) {
         super(context, resource, textViewResourceId, objects);
+        mContext = context;
         mTypeface = Typeface.createFromAsset(getContext().getAssets(), "fonts/Teko-Regular.ttf");
     }
 
     CustomArrayAdapter(Context context, int resource, List<String> objects) {
         super(context, resource, objects);
+        mContext = context;
         mTypeface = Typeface.createFromAsset(getContext().getAssets(), "fonts/Teko-Regular.ttf");
     }
 
     CustomArrayAdapter(Context context, int resource, int textViewResourceId, List<String> objects) {
         super(context, resource, textViewResourceId, objects);
+        mContext = context;
         mTypeface = Typeface.createFromAsset(getContext().getAssets(), "fonts/Teko-Regular.ttf");
     }
 
@@ -49,7 +57,8 @@ class CustomArrayAdapter extends ArrayAdapter<String> {
     public @NonNull View getView(int position, View convertView, @NonNull ViewGroup parent) {
         TextView view = (TextView) super.getView(position, convertView, parent);
         view.setTypeface(mTypeface);
-        view.setTextSize(24); // sp
+        view.setTextSize(TypedValue.COMPLEX_UNIT_PX,
+                mContext.getResources().getDimension(R.dimen.spinner_font_size));
         return view;
     }
 
@@ -58,7 +67,8 @@ class CustomArrayAdapter extends ArrayAdapter<String> {
     public @NonNull View getDropDownView(int position, View convertView, @NonNull ViewGroup parent) {
         TextView view = (TextView) super.getDropDownView(position, convertView, parent);
         view.setTypeface(mTypeface);
-        view.setTextSize(24); // sp
+        view.setTextSize(TypedValue.COMPLEX_UNIT_PX,
+                mContext.getResources().getDimension(R.dimen.spinner_font_size));
         return view;
     }
 }
