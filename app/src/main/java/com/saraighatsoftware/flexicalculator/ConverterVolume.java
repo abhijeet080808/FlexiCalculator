20ba73ebe7279a849dc3aaf4f80d9542f58d1a6e
Abhijeet Baruah <abhijeet080808@gmail.com>
2017-05-26 09:46:35 +0530

    Use BigFraction to convert

--- app/src/main/java/com/saraighatsoftware/flexicalculator/ConverterVolume.java
+++ app/src/main/java/com/saraighatsoftware/flexicalculator/ConverterVolume.java
@@ -12,9 +12,7 @@ import java.util.List;
 
 class ConverterVolume {
 
-    private static final int INTERNAL_SCALE = 12;
-
-    private enum Type {
+    private enum VolumeUnit {
         MILLILITERS,
         LITERS,
         CUBIC_CENTIMETERS,
@@ -32,6 +30,7 @@ class ConverterVolume {
         TEASPOONS_UK,
         TABLESPOONS_UK,
         FLUID_OUNCES_UK,
+        CUPS_UK,
         PINTS_UK,
         QUARTS_UK,
         GALLONS_UK
@@ -39,10 +38,10 @@ class ConverterVolume {
 
     private class ConversionPair {
 
-        final Type input;
-        final Type output;
+        final VolumeUnit input;
+        final VolumeUnit output;
 
-        ConversionPair(Type input, Type output) {
+        ConversionPair(VolumeUnit input, VolumeUnit output) {
             this.input = input;
             this.output = output;
         }
@@ -70,39 +69,79 @@ class ConverterVolume {
 
     private String[] mUnits;
     // output = input * conversion_factor
-    private HashMap<ConversionPair, BigDecimal> mConversionFactors;
+    private HashMap<ConversionPair, BigFraction> mConversionFactors;
 
     ConverterVolume(Context context) {
         mUnits = context.getResources().getStringArray(R.array.volume);
-        // http://www.metric-conversions.org/volume/milliliters-conversion.htm
-        mConversionFactors.put(new ConversionPair(Type.MILLILITERS, Type.LITERS), new BigDecimal("0.001"));
-        mConversionFactors.put(new ConversionPair(Type.MILLILITERS, Type.CUBIC_CENTIMETERS), new BigDecimal("1"));
-        mConversionFactors.put(new ConversionPair(Type.MILLILITERS, Type.CUBIC_METERS), new BigDecimal("0.000001"));
-        mConversionFactors.put(new ConversionPair(Type.MILLILITERS, Type.CUBIC_INCHES), new BigDecimal("0.061023744"));
-        mConversionFactors.put(new ConversionPair(Type.MILLILITERS, Type.CUBIC_FEET), new BigDecimal("0.000035314667"));
-        mConversionFactors.put(new ConversionPair(Type.MILLILITERS, Type.CUBIC_YARDS), new BigDecimal("0.0000013079506"));
-        mConversionFactors.put(new ConversionPair(Type.MILLILITERS, Type.TEASPOONS_US), new BigDecimal("0.20288414"));
-        mConversionFactors.put(new ConversionPair(Type.MILLILITERS, Type.TABLESPOONS_US), new BigDecimal("0.067628045"));
-        mConversionFactors.put(new ConversionPair(Type.MILLILITERS, Type.FLUID_OUNCES_US), new BigDecimal("0.033814023"));
-        mConversionFactors.put(new ConversionPair(Type.MILLILITERS, Type.CUPS_US), new BigDecimal("0.0042267528"));
-        mConversionFactors.put(new ConversionPair(Type.MILLILITERS, Type.PINTS_US), new BigDecimal("0.0021133764"));
-        mConversionFactors.put(new ConversionPair(Type.MILLILITERS, Type.QUARTS_US), new BigDecimal("0.0010566882"));
-        mConversionFactors.put(new ConversionPair(Type.MILLILITERS, Type.GALLONS_US), new BigDecimal("0.00026417205"));
-        mConversionFactors.put(new ConversionPair(Type.MILLILITERS, Type.TEASPOONS_UK), new BigDecimal("0.28156000"));
-        mConversionFactors.put(new ConversionPair(Type.MILLILITERS, Type.TABLESPOONS_UK), new BigDecimal("0.070390100"));
-        mConversionFactors.put(new ConversionPair(Type.MILLILITERS, Type.FLUID_OUNCES_UK), new BigDecimal("0.035195080"));
-        mConversionFactors.put(new ConversionPair(Type.MILLILITERS, Type.PINTS_UK), new BigDecimal("0.0017597540"));
-        mConversionFactors.put(new ConversionPair(Type.MILLILITERS, Type.QUARTS_UK), new BigDecimal("0.00087987699"));
-        mConversionFactors.put(new ConversionPair(Type.MILLILITERS, Type.GALLONS_UK), new BigDecimal("0.00021996925"));
+
+        mConversionFactors = new HashMap<>();
+
+        mConversionFactors.put(new ConversionPair(VolumeUnit.MILLILITERS, VolumeUnit.LITERS),
+                new BigFraction(1L, 1000L));
+        mConversionFactors.put(new ConversionPair(VolumeUnit.MILLILITERS, VolumeUnit.CUBIC_CENTIMETERS),
+                new BigFraction(1L, 1L));
+        mConversionFactors.put(new ConversionPair(VolumeUnit.MILLILITERS, VolumeUnit.CUBIC_METERS),
+                new BigFraction(1L, 1000000L));
+        // 127 cm = 50 inches
+        mConversionFactors.put(new ConversionPair(VolumeUnit.MILLILITERS, VolumeUnit.CUBIC_INCHES),
+                new BigFraction(125000L, 2048383L));
+        // 762 cm = 25 feet
+        mConversionFactors.put(new ConversionPair(VolumeUnit.MILLILITERS, VolumeUnit.CUBIC_FEET),
+                new BigFraction(15625L, 442450728L));
+        // 2286 cm = 25 yards
+        mConversionFactors.put(new ConversionPair(VolumeUnit.MILLILITERS, VolumeUnit.CUBIC_YARDS),
+                new BigFraction(15625L, 11946169656L));
+        // 77 ci3 = 256 us teaspoon
+        mConversionFactors.put(new ConversionPair(VolumeUnit.MILLILITERS, VolumeUnit.TEASPOONS_US),
+                new BigFraction(32000000L, 157725491L));
+        // 3 us teaspoon = 1 us tablespoon
+        mConversionFactors.put(new ConversionPair(VolumeUnit.MILLILITERS, VolumeUnit.TABLESPOONS_US),
+                new BigFraction(32000000L, 473176473L));
+        // 2 us tablespoon = 1 us fluid ounce
+        mConversionFactors.put(new ConversionPair(VolumeUnit.MILLILITERS, VolumeUnit.FLUID_OUNCES_US),
+                new BigFraction(32000000L, 946352946L));
+        // 8 us fluid ounce = 1 us cup
+        mConversionFactors.put(new ConversionPair(VolumeUnit.MILLILITERS, VolumeUnit.CUPS_US),
+                new BigFraction(32000000L, 7570823568L));
+        // 2 us cup = 1 us pint
+        mConversionFactors.put(new ConversionPair(VolumeUnit.MILLILITERS, VolumeUnit.PINTS_US),
+                new BigFraction(32000000L, 15141647136L));
+        // 2 us pint = 1 us quart
+        mConversionFactors.put(new ConversionPair(VolumeUnit.MILLILITERS, VolumeUnit.QUARTS_US),
+                new BigFraction(32000000L, 30283294272L));
+        // 4 us quart = 1 us gallon
+        mConversionFactors.put(new ConversionPair(VolumeUnit.MILLILITERS, VolumeUnit.GALLONS_US),
+                new BigFraction(32000000L, 121133177088L));
+        // 28.4130625 ml = 1 uk fluid ounce
+        // 5 uk fluid ounce = 24 uk teaspoon
+        mConversionFactors.put(new ConversionPair(VolumeUnit.MILLILITERS, VolumeUnit.TEASPOONS_UK),
+                new BigFraction(240000000L, 1420653125L));
+        // 3 uk teaspoon = 1 uk tablespoon
+        mConversionFactors.put(new ConversionPair(VolumeUnit.MILLILITERS, VolumeUnit.TABLESPOONS_UK),
+                new BigFraction(240000000L, 4261959375L));
+        // 28.4130625 ml = 1 uk fluid ounce
+        mConversionFactors.put(new ConversionPair(VolumeUnit.MILLILITERS, VolumeUnit.FLUID_OUNCES_UK),
+                new BigFraction(10000000L, 284130625L));
+        // 10 uk fluid ounce - 1 uk cup
+        mConversionFactors.put(new ConversionPair(VolumeUnit.MILLILITERS, VolumeUnit.CUPS_UK),
+                new BigFraction(10000000L, 2841306250L));
+        // 2 uk cup - 1 uk pint
+        mConversionFactors.put(new ConversionPair(VolumeUnit.MILLILITERS, VolumeUnit.PINTS_UK),
+                new BigFraction(10000000L, 5682612500L));
+        // 2 uk pint = 1 uk quart
+        mConversionFactors.put(new ConversionPair(VolumeUnit.MILLILITERS, VolumeUnit.QUARTS_UK),
+                new BigFraction(10000000L, 11365225000L));
+        // 4 uk quart = 1 uk gallon
+        mConversionFactors.put(new ConversionPair(VolumeUnit.MILLILITERS, VolumeUnit.GALLONS_UK),
+                new BigFraction(10000000L, 45460900000L));
     }
 
     List<String> GetUnits() {
         return Arrays.asList(mUnits);
     }
 
-    static BigFraction ToBigFraction(BigDecimal val) {
-        int scale = val.scale();
-
+    private static BigFraction ToBigFraction(BigDecimal val) {
+        final int scale = val.scale();
         // If scale >= 0 then the value is val.unscaledValue() / 10^scale
         if(scale >= 0)
             return new BigFraction(val.unscaledValue(), BigInteger.TEN.pow(scale));
@@ -110,24 +149,22 @@ class ConverterVolume {
         return new BigFraction(val.unscaledValue().multiply(BigInteger.TEN.pow(-scale)));
     }
 
-    String Convert(String input, Type inputType, Type outputType) {
+    String Convert(String value, VolumeUnit input, VolumeUnit output) {
         try {
             // convert to milliliters first
-            BigDecimal in = new BigDecimal(input);
-            if (inputType != Type.MILLILITERS) {
-                in = in.divide(
-                        mConversionFactors.get(new ConversionPair(Type.MILLILITERS, inputType)),
-                        INTERNAL_SCALE,
-                        BigDecimal.ROUND_HALF_EVEN);
+            BigFraction in = ToBigFraction(new BigDecimal(value));
+            if (input != VolumeUnit.MILLILITERS) {
+                in = in.divide(mConversionFactors.get(
+                        new ConversionPair(VolumeUnit.MILLILITERS, input)));
             }
             // convert to output format
-            BigDecimal out = in;
-            if (outputType != Type.MILLILITERS) {
-                out = out.multiply(
-                        mConversionFactors.get(new ConversionPair(Type.MILLILITERS, outputType)));
+            BigFraction out = in;
+            if (output != VolumeUnit.MILLILITERS) {
+                out = out.multiply(mConversionFactors.get(
+                        new ConversionPair(VolumeUnit.MILLILITERS, output)));
             }
 
-            return ResultFormat.Format(out);
+            return ResultFormat.Format(out.bigDecimalValue(BigDecimal.ROUND_HALF_EVEN));
         }
         catch (Exception e) {
             return "0";
